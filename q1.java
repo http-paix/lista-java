9135b4a120240024f9e1fba03bd89ef6075d7d15
Adrielly Paixão <108769339+http-paix@users.noreply.github.com>
2024-12-12 09:48:50 -0300

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class q1 {
+    // Faça um programa que peça as 4 notas bimestrais de um estudante e mostre a média aritmética entre elas.
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+
+        float somatorio = 0;
+        for (int i = 1; i<=4; i++) {
+            System.out.printf("Digite a %da nota: ", i);
+            float nota = tec.nextFloat();
+
+            somatorio += nota;
+        }
+
+        double media = somatorio / 4;
+
+        System.out.println("A média aritmética das 4 notas bimestrais do estudante é: " + media);
+
+        tec.close();
+    }
+}
